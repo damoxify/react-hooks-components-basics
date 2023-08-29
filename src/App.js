da67be67b9fa5066dedf747352c41ff1c04e0a06
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-29 15:38:01 +0100

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Article />
       <Comment />
     </div>
